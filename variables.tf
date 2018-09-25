93e7f2f4e3860489169114e2e76b65dd952c7aa9
King Chung Huang <kinghuang@mac.com>
2018-09-25 09:20:31 -0600

    Add variables for network ACLs
    Add variables for specifying network ACLs for public, private, and
    intra subnets. The ACLs are defined in a list, with sets of seven
    elements for the rule number, rule action, from port, to port,
    protocol, and cidr block.

--- variables.tf
+++ variables.tf
@@ -102,6 +102,36 @@ variable "intra_subnets" {
   default     = []
 }
 
+variable "public_inbound_acls" {
+  description = "Public subnets inbound network ACLs"
+  default     = ["100", "allow", 0, 0, "-1", "0.0.0.0/0", "Allow ALL inbound"]
+}
+
+variable "public_outbound_acls" {
+  description = "Public subnets outbound network ACLs"
+  default     = ["100", "allow", 0, 0, "-1", "0.0.0.0/0", "Allow ALL outbound"]
+}
+
+variable "private_inbound_acls" {
+  description = "Private subnets inbound network ACLs"
+  default     = ["100", "allow", 0, 0, "-1", "0.0.0.0/0", "Allow ALL inbound"]
+}
+
+variable "private_outbound_acls" {
+  description = "Private subnets outbound network ACLs"
+  default     = ["100", "allow", 0, 0, "-1", "0.0.0.0/0", "Allow ALL outbound"]
+}
+
+variable "intra_inbound_acls" {
+  description = "Intra subnets inbound network ACLs"
+  default     = ["100", "allow", 0, 0, "-1", "0.0.0.0/0", "Allow ALL inbound"]
+}
+
+variable "intra_outbound_acls" {
+  description = "Intra subnets outbound network ACLs"
+  default     = ["100", "allow", 0, 0, "-1", "0.0.0.0/0", "Allow ALL outbound"]
+}
+
 variable "create_database_subnet_group" {
   description = "Controls if database subnet group should be created"
   default     = true
